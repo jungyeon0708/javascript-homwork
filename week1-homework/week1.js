7932cc4f52d3ebf603d64be9a280f2c54e2fa7fb
jungyeon0708 <kimkjy0708@gmail.com>
2025-06-13 10:15:48 +0900

    [과제] 변수, 상수 작성과제 -md, index 파일 생성 - 1번 과제 완료

---
+++ week1-homework/week1.js
@@ -0,0 +1,34 @@
+//  변수, 상수 작성 과제
+
+// 각 항목에 대해 변수나 상수를 선언하고, 알맞은 값을 할당하세요.
+
+// 1. 사용자 이름을 저장할 변수를 선언하고 "자신의 이름"을 할당하세요.
+let userName = "김정연";
+console.log(userName);
+// 2. "절대 영도(온도의 최저 한계)인 -273.15°C"를 담을 상수를 선언하세요.
+const ABSOLUTE_ZERO = -273.15;
+console.log(ABSOLUTE_ZERO + "°C");
+// 3. "로그인 여부"를 확인하는 불리언 타입 변수을 선언하고 적절한값을 할당하세요.
+let isLoggedIn = true;
+console.log(isLoggedIn);
+// 4. "사용자 나이" 변수를 선언하고 숫자 값을 할당하세요.
+let userAge = 30;
+console.log(userAge + "살");
+// 5. "상품 가격(예: 39,900원)"을 담는 변수를 작성하세요.
+let productPrice = "39,900";
+console.log(productPrice + "원");
+// 6. 웹 페이지의 "배경색"을 담는 상수를 작성하고 적절한 값을 할당하세요.
+const backGroundColor = "#fff";
+console.log(backGroundColor);
+// 7. 사용자가 작성한 "댓글 수(예: 12)"를 담는 변수를 선언하고 값을 할당하세요.
+let commentAcount = 12;
+console.log("댓글 수 : " + commentAcount + "개");
+// 8. "현재 페이지 번호(예: 3)"를 담는 변수를 작성하고 적절한 값을 할당하세요.
+let pageNumber = 3;
+console.log(pageNumber + "페이지");
+// 9. "회원 등급('VIP', '골드', '실버' 중 하나)"을 담는 변수를 작성해보세요.
+let memberGrade = "VIP";
+console.log("회원등급 : " + memberGrade);
+// 10. "버튼이 클릭되었는지 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isButtonClicked = true;
+console.log(isButtonClicked);
